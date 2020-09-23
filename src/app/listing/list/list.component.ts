bdefba4194d49bf79348d7508af91b6cc9a6bfb6
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-09-23 14:24:52 +0600

    Cart Update

--- src/app/listing/list/list.component.ts
+++ src/app/listing/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit , Input } from '@angular/core';
+import {ItemService} from '../../services/item.service';
 
 @Component({
   selector: 'app-list',
@@ -9,13 +10,38 @@ export class ListComponent implements OnInit {
 
   @Input() items =[];
 
-  constructor() { }
+  allItems = [] ;
+
+  constructor(private itemService : ItemService) { }
 
   ngOnInit(): void {
+      this.itemService.getProducts().subscribe(data=>{
+
+        this.allItems = data ;
+
+      });
 
   }
 
   onUpdate(){
+
+
+    var itm = this.allItems ;
+
+    for(let it of itm){
+
+      if(it.order>0  &&  ( it.confirmed + it.order < it.stock) ){
+
+        var val = it.order ;
+        it.order = 0 ;
+        it.confirmed +=val ;
+
+      }
+
+    }
+
+    this.itemService.setProducts(itm) ;
+
 
   }
 
